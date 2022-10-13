7ea81333b1b64eab07769ea2d2b0125e23b6ba00
BenaventeX24 <benaventematias04@gmail.com>
2022-10-13 12:53:29 -0300

    script update

--- FaroApi/FaroDB.sql
+++ FaroApi/FaroDB.sql
@@ -222,7 +222,7 @@ call DBFiller_Career_VinculateKeyword("Silvicultura",7);
 insert into CENTRE_CAREER values(3,5);
 insert into CENTRE_CAREER values(3,6);
 
-create or replace view CENTRES_VW as select idCentre, centreName, free, addressStreet, addressNumber, latitude, longitude, phoneNumber, schoolarLevel, group_concat(centreSchedule) as centreSchedules from CENTRE natural left join CENTRE_SCHEDULES natural left join schoolarlevel natural left join SCHEDULES group by (idCentre);
+create or replace view CENTRES_VW as select idCentre, centreName, free, addressStreet, addressNumber, latitude, longitude, phoneNumber, schoolarLevel, group_concat(centreSchedule) as centreSchedules from CENTRE natural left join CENTRE_SCHEDULES natural left join SCHOOLARLEVEL natural left join SCHEDULES group by (idCentre);
 create or replace view CAREERS_VW as select idCareer, careerName, careerDescription, degree, duration, group_concat(keyword) as keywords from CAREER natural  join CAREER_KEYWORD natural join KEYWORD group by idCareer;
 
 drop user if exists 'DBAdmin'
