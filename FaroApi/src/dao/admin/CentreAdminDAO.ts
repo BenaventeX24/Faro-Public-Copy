eb36959706f47411182829fa607784a04a668111
BenaventeX24 <benaventematias04@gmail.com>
2022-10-18 10:14:08 -0300

    fix

--- FaroApi/src/dao/admin/CentreAdminDAO.ts
+++ FaroApi/src/dao/admin/CentreAdminDAO.ts
@@ -48,7 +48,7 @@ export class CentreAdminDAO {
           centre.getPhoneNumber(),
         ],
         async (err, res) => {
-          if (err) reject(err + "error");
+          if (err) reject("RAZONE'");
           else {
             /*For every given schedule vinculate it to the centre*/
             await this.vinculateCentreSchedules(
