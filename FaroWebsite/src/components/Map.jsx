5008889f6847119a3a86f6c65fb2233f2513437f
lucasscurtoo <lucascurtoo@gmail.com>
2022-10-20 10:10:20 -0300

    Changing translate options, logoDark update for a better markers view

--- FaroWebsite/src/components/Map.jsx
+++ FaroWebsite/src/components/Map.jsx
@@ -19,7 +19,6 @@ function MyComponent({filterCentre, filters}) {
   const [map, setMap] = useState(null);
   const [markers, setMarkers] = useState(null);
   const [modalInfo, setModalInfo] = useState(false);
-  const [loadMap, setLoadMap] = useState(false);
 
   useEffect(() =>{
     const getCentresCoords = async () => {
@@ -40,13 +39,9 @@ function MyComponent({filterCentre, filters}) {
     }
   },[filterCentre])
 
-  useEffect(() =>{
-   setLoadMap(true)
-  },[])
-
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: "AIzaSyCIfWaZPMHJaqzMX6R36zyz0-8RenAzKyo",
+    googleMapsApiKey: "AIzaSyDVaCzLPWA93p0XZGS9c10vosPoTH_bH2o",
   });
 
   const onLoad = useCallback(async function callback(map) {
@@ -101,7 +96,7 @@ function MyComponent({filterCentre, filters}) {
       )}
       <GoogleMap
         mapContainerStyle={containerStyle}
-        //onUnmount={onUnmount}
+        onUnmount={onUnmount}
         zoom={12}
         center={center}
         options={myOptions}
