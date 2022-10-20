f32324a6c00e677755c5a6b8792cd613144435c8
BenaventeX24 <benaventematias04@gmail.com>
2022-10-20 12:39:23 -0300

    Update get centres by filter

--- FaroApi/src/services/GetCentreService.ts
+++ FaroApi/src/services/GetCentreService.ts
@@ -23,23 +23,23 @@ export const getCentreByParams = (query): Promise<Centre | any> => {
         }
       );
     } else {
-      let queryFilter = "select idCentre, centreName from CENTRES_VW natural join CENTRE_CAREER natural join CAREERS_VW where ";
+      let queryFilter = "select DISTINCT idCentre, centreName from CENTRES_VW natural join CENTRE_CAREER natural join CAREERS_VW where ";
       const queryParams = []
 
       queryFilter = queryFilter + (query.free ? "free=? " : "free=free ");
-      if(query.free) query.free==='true' ? queryParams.push(true) : queryParams.push(false)
+      if(query.free) query.free==='true' ? queryParams.push(true) : queryParams.push(false);
 
       queryFilter = queryFilter + (query.schoolarLevel ? "and schoolarLevel=? " : "and schoolarLevel=schoolarLevel ");
-      if(query.schoolarLevel) queryParams.push(query.schoolarLevel)
+      if(query.schoolarLevel) queryParams.push(query.schoolarLevel);
 
       queryFilter = queryFilter + (query.centreSchedules ? "and centreSchedules like ? " : "and centreSchedules = centreSchedules ");
-      if(query.centreSchedules) queryParams.push("%" + query.centreSchedules + "%")
+      if(query.centreSchedules) queryParams.push("%" + query.centreSchedules + "%");
 
       queryFilter = queryFilter + (query.idCareer ? "and idCareer = ? " : "and idCareer = idCareer ");
-      if(query.idCareer) queryParams.push(query.idCareer)
+      if(query.idCareer) queryParams.push(query.idCareer);
 
       queryFilter = queryFilter + (query.keyword ? "and keywords like ? " : "and keywords=keywords ");
-      if(query.keyword) queryParams.push("%" + query.keyword + "%")
+      if(query.keyword) queryParams.push("%" + query.keyword + "%");
 
       centreDB
         .getCentresByFilter(
